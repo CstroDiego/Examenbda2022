2deed1da507be3fd3486eda70c3842e22e803c34
diego <diego.castro@potros.itson.edu.mx>
2022-05-08 12:03:59 -0700

    Se crearon los metodos de persistencia para Cuenta y movimiento

--- src/mx/itson/nu/persistencia/CuentaDAO.java
+++ src/mx/itson/nu/persistencia/CuentaDAO.java
@@ -1,4 +1,114 @@
 package mx.itson.nu.persistencia;
 
+import mx.itson.nu.entidades.Cliente;
+import mx.itson.nu.entidades.Cuenta;
+import mx.itson.nu.utilerias.HibernateUtil;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+
+import javax.persistence.criteria.CriteriaQuery;
+import java.util.ArrayList;
+import java.util.List;
+
 public class CuentaDAO {
+
+
+  public static List<Cuenta> obtenerTodos() {
+    List<Cuenta> cuentas = new ArrayList<>();
+    try {
+      Session session = HibernateUtil.getSessionFactory().openSession();
+      CriteriaQuery<Cuenta> criteriaQuery = session.getCriteriaBuilder().createQuery(Cuenta.class);
+      criteriaQuery.from(Cuenta.class);
+      cuentas = session.createQuery(criteriaQuery).getResultList();
+    } catch (HibernateException ex) {
+      System.err.println("Error: " + ex.getMessage());
+    }
+    return cuentas;
+  }
+
+
+  public static boolean guardar(
+      float credito,
+      float ajusteCredito,
+      float intereses,
+      boolean bloquearTarjeta,
+      Cliente cliente) {
+    boolean resultado = false;
+    try {
+      Session session = HibernateUtil.getSessionFactory().openSession();
+      session.beginTransaction();
+      Cuenta c = new Cuenta();
+      c.setCredito(credito);
+      c.setAjusteCredito(ajusteCredito);
+      c.setIntereses(intereses);
+      c.setBloquearTarjeta(bloquearTarjeta);
+      c.setCliente(cliente);
+      session.save(c);
+      session.getTransaction().commit();
+      resultado = c.getId() != 0;
+    } catch (HibernateException ex) {
+      System.err.println("Error al guardar cuenta: " + ex.getMessage());
+    }
+    return resultado;
+  }
+
+
+  public static boolean editar(
+      int id,
+      float credito,
+      float ajusteCredito,
+      float intereses,
+      boolean bloquearTarjeta,
+      Cliente cliente) {
+    boolean resultado = false;
+    try {
+      Session session = HibernateUtil.getSessionFactory().openSession();
+      session.beginTransaction();
+      Cuenta c = obtenerPorId(id);
+      if (c != null) {
+        c.setCredito(credito);
+        c.setAjusteCredito(ajusteCredito);
+        c.setIntereses(intereses);
+        c.setBloquearTarjeta(bloquearTarjeta);
+        c.setCliente(cliente);
+        session.saveOrUpdate(c);
+        session.getTransaction().commit();
+        resultado = c.getId() != 0;
+      }
+    } catch (HibernateException ex) {
+      System.err.println("Error al editar cuenta: " + ex.getMessage());
+    }
+    return resultado;
+  }
+
+
+  public static boolean eliminar(int id) {
+    boolean resultado = false;
+    try {
+      Session session = HibernateUtil.getSessionFactory().openSession();
+      session.beginTransaction();
+      Cuenta c = obtenerPorId(id);
+      if (c != null) {
+        session.delete(c);
+        session.getTransaction().commit();
+        resultado = c.getId() != 0;
+      }
+    } catch (HibernateException ex) {
+      System.err.println("Error al eliminar cuenta: " + ex.getMessage());
+    }
+    return resultado;
+  }
+
+
+  public static Cuenta obtenerPorId(int id) {
+    Cuenta c = null;
+    try {
+      Session session = HibernateUtil.getSessionFactory().openSession();
+      session.beginTransaction();
+      c = session.get(Cuenta.class, id);
+    } catch (HibernateException ex) {
+      System.err.println("Error al obtener cuenta: " + ex.getMessage());
+    }
+    return c;
+  }
 }
--- src/mx/itson/nu/persistencia/MovimientoDAO.java
+++ src/mx/itson/nu/persistencia/MovimientoDAO.java
@@ -1,4 +1,104 @@
 package mx.itson.nu.persistencia;
 
+import mx.itson.nu.entidades.Cuenta;
+import mx.itson.nu.entidades.Movimiento;
+import mx.itson.nu.utilerias.HibernateUtil;
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
+
+import javax.persistence.criteria.CriteriaQuery;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
 public class MovimientoDAO {
+
+  public static List<Movimiento> obtenerTodos() {
+    List<Movimiento> movimientos = new ArrayList<>();
+    try {
+      Session session = HibernateUtil.getSessionFactory().openSession();
+      CriteriaQuery<Movimiento> criteriaQuery =
+          session.getCriteriaBuilder().createQuery(Movimiento.class);
+      criteriaQuery.from(Movimiento.class);
+      movimientos = session.createQuery(criteriaQuery).getResultList();
+    } catch (HibernateException ex) {
+      System.err.println("Error: " + ex.getMessage());
+    }
+    return movimientos;
+  }
+
+  public static boolean guardar(
+      Date fecha, float monto, String tipo, String concepto, Cuenta cuenta) {
+    boolean resultado = false;
+    try {
+      Session session = HibernateUtil.getSessionFactory().openSession();
+      session.beginTransaction();
+      Movimiento m = new Movimiento();
+      m.setFecha(fecha);
+      m.setMonto(monto);
+      m.setTipo(tipo);
+      m.setConcepto(concepto);
+      m.setCuenta(cuenta);
+      session.save(m);
+      session.getTransaction().commit();
+      resultado = m.getId() != 0;
+    } catch (HibernateException ex) {
+      System.err.println("Error al guardar movimiento: " + ex.getMessage());
+    }
+    return resultado;
+  }
+
+  public static boolean editar(
+      int id, Date fecha, float monto, String tipo, String concepto, Cuenta cuenta) {
+    boolean resultado = false;
+    try {
+      Session session = HibernateUtil.getSessionFactory().openSession();
+      session.beginTransaction();
+      Movimiento m = obtenerPorId(id);
+      if (m != null) {
+        m.setFecha(fecha);
+        m.setMonto(monto);
+        m.setTipo(tipo);
+        m.setConcepto(concepto);
+        m.setCuenta(cuenta);
+        session.saveOrUpdate(m);
+        session.getTransaction().commit();
+        resultado = m.getId() != 0;
+      }
+    } catch (HibernateException ex) {
+      System.err.println("Error al editar movimiento: " + ex.getMessage());
+    }
+    return resultado;
+  }
+
+  public static boolean eliminar(int id) {
+    boolean resultado = false;
+    try {
+      Session session = HibernateUtil.getSessionFactory().openSession();
+      session.beginTransaction();
+      Movimiento m = obtenerPorId(id);
+
+      if (m != null) {
+        session.delete(m);
+        session.getTransaction().commit();
+        resultado = m.getId() != 0;
+      }
+
+    } catch (HibernateException ex) {
+      System.err.println("Error al eliminar movimiento: " + ex.getMessage());
+    }
+    return resultado;
+  }
+
+  public static Movimiento obtenerPorId(int id) {
+    Movimiento m = null;
+    try {
+      Session session = HibernateUtil.getSessionFactory().openSession();
+      session.beginTransaction();
+      m = session.get(Movimiento.class, id);
+    } catch (HibernateException ex) {
+      System.err.println("Error al obtener movimiento: " + ex.getMessage());
+    }
+    return m;
+  }
 }
